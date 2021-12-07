bea3711f9f026810487dd3a353b4368c09c3486e
Christina Fuchs <christina.fuchs@ili.fau.de>
2021-12-07 09:44:41 +0100

    update ilias min and max version

--- plugin.php
+++ plugin.php
@@ -9,8 +9,8 @@ $version = "1.3.2";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.4.4";
-$ilias_max_version = "5.4.999";
+$ilias_min_version = "7.0";
+$ilias_max_version = "7.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann";
