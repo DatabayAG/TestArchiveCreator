844e06cd30d3229eb0508f4230bf91eef868ce04
Fred Neumann <fred.neumann@fau.de>
2023-04-25 12:46:03 +0200

    new branch for ILIAS 8

--- plugin.php
+++ plugin.php
@@ -5,12 +5,12 @@
 $id = "tarc_ui";
 
 // code version; must be changed for all code changes
-$version = "1.4.2";
+$version = "1.5.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "7.0";
-$ilias_max_version = "7.999";
+$ilias_min_version = "8.0";
+$ilias_max_version = "8.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann";
